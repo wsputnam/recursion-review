82ce2d54f11e4b95a1386a4b4685ad12dfe7d395
wsputnam <williamputnamjr@gmail.com>
2017-11-27 12:34:43 -0800

    passed first few tests of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+   return ''+obj;
+  }
+  if (typeof obj === 'string') {
+   return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+   var arr = [];
+   obj.forEach(function(item, collection, index) {
+   //if (index > 0) {
+    //str = str.concat(',');
+   //}
+    arr.push(stringifyJSON(item));
+   //str = str.concat(stringifyJSON(item));
+     
+   });
+   var str = arr.join(',');
+   return '[' + str + ']';
+  }
+  if (typeof obj === 'object') {
+   var str = '{';
+   for (var key in obj) {
+    
+   }
+  }
+
+
 };
